fc4bc435192f06b8c4457cad5b76e35e57ff88c5
hakkamar <hakkis68@gmail.com>
2018-10-03 10:07:07 +0300

    5.13 blogilistan testit, osa 2

--- bloglist-frontend/src/components/SimpleBlog.test.js
+++ bloglist-frontend/src/components/SimpleBlog.test.js
@@ -21,4 +21,49 @@ describe.only('<SimpleBlog />', () => {
     expect(blogiDiv.text()).toContain(blog.author)
     expect(liketDiv.text()).toContain(blog.likes)
   })
+  it('clicking the button calls event handler once', () => {
+    const blog = {
+      title: 'Testi Title',
+      author: 'Herra Hakkarainen',
+      url: 'http://www.jotain.com',
+      likes: 3
+    }
+  
+    const mockHandler = jest.fn()
+  
+    const blogComponent = shallow(
+      <SimpleBlog
+        blog={blog}
+        onClick={mockHandler}
+      />
+    )
+  
+    const button = blogComponent.find('button')
+    button.simulate('click')
+  
+    expect(mockHandler.mock.calls.length).toBe(1)
+  })
+  it('clicking the button twice calls event handler twice', () => {
+    const blog = {
+      title: 'Testi Title',
+      author: 'Herra Hakkarainen',
+      url: 'http://www.jotain.com',
+      likes: 3
+    }
+  
+    const mockHandler = jest.fn()
+  
+    const blogComponent = shallow(
+      <SimpleBlog
+        blog={blog}
+        onClick={mockHandler}
+      />
+    )
+  
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+  
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
